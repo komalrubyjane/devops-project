daa773b67ab23ad143bbbf20fa2d189e79c7b2f4
komalrubyjane <23h51a0539@cmrcet.ac.in>
2025-11-15 14:09:53 +0530

    ini push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 3000;
 
 const requestHandler = (req, res) => {
-  res.end("CI/CD Node.js App deployed via Jenkins 🚀");
+  res.end("CI/CD Node.js App deployed via Jenkins fijriieiri🚀");
 };
 
 const server = http.createServer(requestHandler);
